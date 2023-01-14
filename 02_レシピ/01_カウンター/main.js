407ba51a1b35cf1a35c81191022111eab7f5538f
MiyuMizuno <miyu.leon.1431@icloud.com>
2023-01-14 20:05:01 +0900

    [カウント]を学習しました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,2 +1,9 @@
 //ボタンを押すと、表示されている数字が１増えます。
-const button = document.getElementById("")
+const button = document.getElementById("plus-button")
+const text = document.getElementById("display")
+let count = 0
+button.onclick = function () {
+  //変数に1を代入
+  count += 1
+  text.textContent = count
+}
